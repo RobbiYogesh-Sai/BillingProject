6b60628a624d2fee2f55c2f63d4f61f3a91d00e8
robbsai <robbsai@LIN-5CG11680BK>
2023-10-23 16:47:26 +0530

    Add project files.

---
+++ DemoBillingProject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingProject
+{
+    public class DemoBillingProject
+    {
+        int p;
+        String[] products=new String[10];
+        int[] units=new int[10];
+        int[] totalUnits=new int[10];
+        public void GetDetails()
+        {
+            Console.Write("Enter how many products taken: ");
+            p=Convert.ToInt32(Console.ReadLine());
+        }
+        public void GetProductDetails()
+        {
+            for(int i = 1; i <= p; i++)
+            {
+                Console.Write($"{i}: Enter the product Name: ");
+                products[i]=Convert.ToString(Console.ReadLine());
+            }
+        }
+        public void GetEachProductCost()
+        {
+            for(int i = 1;i <= p; i++)
+            {
+                Console.WriteLine($"{i}: Enter the Each Product Cost: ");
+                units[i]=Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void DispalyProduct()
+        {
+            for (int i = 1; i <= p; i++)
+            {
+                Console.WriteLine(products[i]);
+                totalUnits[i] = units[i] + units[i + 1];
+            }
+            Console.WriteLine("\n");
+        }
+        public void TotalPrice()
+        {
+            Console.WriteLine("The Total Price\n");
+            for (int i = 1; i <= p; i++)
+            {
+                Console.WriteLine($"Total: {totalUnits[i]}");
+                break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BillingProject;
+
+DemoBillingProject obj = new DemoBillingProject();
+obj.GetDetails();
+obj.GetProductDetails();
+obj.GetEachProductCost();
+obj.DispalyProduct();
+obj.TotalPrice();
